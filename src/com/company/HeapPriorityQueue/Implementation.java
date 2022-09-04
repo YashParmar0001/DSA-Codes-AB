6fd532268ed2dcfe8474838e228bc29fe0150f48
Yash Parmar <yashparmar009@outlook.com>
2022-09-04 19:31:27 +0530

    Comments added!

--- src/com/company/HeapPriorityQueue/Implementation.java
+++ src/com/company/HeapPriorityQueue/Implementation.java
@@ -2,15 +2,29 @@ package com.company.HeapPriorityQueue;
 
 public class Implementation {
     public static void main(String[] args) throws Exception {
-        MinHeap heap = new MinHeap(8);
-        heap.insert(5);
-        heap.insert(10);
-        heap.insert(15);
-        heap.insert(12);
-        heap.insert(14);
-        heap.insert(22);
-        System.out.println(heap.extractMin());
+//        MinHeap heap = new MinHeap(8);
+//        heap.insert(5);
+//        heap.insert(10);
+//        heap.insert(15);
+//        heap.insert(12);
+//        heap.insert(14);
+//        heap.insert(22);
 //        System.out.println(heap.extractMin());
+//        System.out.println(heap.extractMin());
+//        heap.printHeap();
+        MaxHeap heap = new MaxHeap(8);
+        heap.insert(25);
+        heap.insert(15);
+        heap.insert(10);
+        heap.insert(30);
+        heap.insert(20);
+        heap.insert(18);
+        System.out.println(heap.extractMax());
+        System.out.println(heap.extractMax());
+        System.out.println(heap.extractMax());
+        System.out.println(heap.extractMax());
+        System.out.println(heap.extractMax());
+        System.out.println(heap.extractMax());
         heap.printHeap();
     }
 }
@@ -78,5 +92,71 @@ class MinHeap {
         for (int i = 1; i <= n; i++) {
             System.out.print(arr[i] + ", ");
         }
+    }
+}
+
+class MaxHeap {
+    private final int[] arr;
+    private final int capacity;
+    private int n;
+
+    public MaxHeap(int capacity) {
+        arr = new int[capacity+1];
+        this.capacity = capacity;
+        n = 0;
+    }
+
+    private void swap(int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public void printHeap() {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+
+    public void insert(int key) throws Exception {
+        if (n == capacity - 1) throw new Exception("Heap is Full!");
+
+        n++;
+        arr[n] = key;
+        int i = n;
+        while (i > 1) {
+            int parent = i / 2;
+            if (arr[i] > arr[parent]) {
+                swap(i, parent);
+                i = parent;
+            }else break;
+        }
+    }
+
+    public int extractMax() throws Exception {
+        if (n == 0) throw new Exception("Heap is Empty!");
+
+        int result = arr[1];
+        arr[1] = arr[n];
+        n--;
+        int i = 1;
+        while (i <= n) {
+            int left = 2 * i;
+            int right = 2 * i + 1;
+
+            int greatest = i;
+            if (left <= n && arr[greatest] < arr[left]) {
+                greatest = left;
+            }
+            if (right <= n && arr[greatest] < arr[right]) {
+                greatest = right;
+            }
+
+            if (greatest != i) {
+                swap(greatest, i);
+                i = greatest;
+            }else break;
+        }
+        return result;
     }
 }
