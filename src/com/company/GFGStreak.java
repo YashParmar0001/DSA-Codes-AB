6217bf4c30625697cb4265f03000172166af0b40
Yash Parmar <yashparmar009@outlook.com>
2022-09-10 18:33:09 +0530

    GFG Streak done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -2,17 +2,10 @@ package com.company;
 
 public class GFGStreak {
     public static void main(String[] args) {
-        System.out.println(digitsNum(5000));
+
     }
 
-    static String digitsNum(int N)
-    {
-        // Code here
-        int temp=N/9;
-        StringBuilder sb=new StringBuilder();
-        if(N%9!=0)sb.append(N%9);
-        sb.append("9".repeat(Math.max(0, temp)));
-        sb.append("0".repeat(Math.max(0, N)));
-        return sb.toString();
+    static long noOfWays(long n){
+        return 2 + 2*(n-1) + (n-2)*(n-1);
     }
 }
