339f13878bbfd6099c262280251edb12fedce25c
Yash Parmar <yashparmar009@outlook.com>
2022-09-12 08:19:52 +0530

    GFG Streak Que done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -5,7 +5,30 @@ public class GFGStreak {
 
     }
 
-    static int MinimumExchange(char[][] matrix) {
-        return 0;
+    public int MinimumExchange(char[][] matrix)
+    {
+        // Code here
+        int x = 0, y = 0;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if((i%2 == 0 && j%2 == 0) || ((i&1)!=0 && (j&1)!=0)){
+                    if(matrix[i][j] == 'A')
+                        x++;
+                }
+                else{
+                    if(matrix[i][j] == 'B')
+                        x++;
+                }
+                if((i%2 == 0 && j%2 == 0) || ((i&1)!=0 && (j&1)!=0)){
+                    if(matrix[i][j] == 'B')
+                        y++;
+                }
+                else{
+                    if(matrix[i][j] == 'A')
+                        y++;
+                }
+            }
+        }
+        return Math.min(x, y);
     }
 }
