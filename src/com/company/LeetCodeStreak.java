f7c3a4f756fc40a236e51cd452e36ee7d41d9fb8
Yash Parmar <yashparmar009@outlook.com>
2022-09-14 12:03:08 +0530

    Leetcode streak done!

--- src/com/company/LeetCodeStreak.java
+++ src/com/company/LeetCodeStreak.java
@@ -1,39 +1,47 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LeetCodeStreak {
     public static void main(String[] args) {
-        System.out.println(validUtf8(new int[]{10}));
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.left.right.right = new TreeNode(1);
+        System.out.println(pseudoPalindromicPaths(root));
     }
 
-    static boolean validUtf8(int[] data) {
-        return util(data, 0);
+    static int pseudoPalindromicPaths (TreeNode root) {
+        Set<Integer> set = new HashSet<>();
+        util2(root, set);
+        return ans;
     }
 
-    static boolean util(int[] data, int index) {
-        if (index >= data.length) return true;
+    static int ans = 0;
 
-        int mask = 1;
-        mask <<= 7;
-        int count = 0;
-        int i = 0;
-//        if ((data[index] & mask) == 0) return true;
-        while (i < 5) {
-            if ((data[index] & mask) == mask) {
-                count++;
-            }else if ((data[index] & mask) == 0) break;
-            mask >>= 1;
-            i++;
+    static void util2(TreeNode root, Set<Integer> set) {
+        if (root == null) return;
+//        Set<Integer> newSet = new HashSet<>(set);
+
+        if (set.contains(root.val)) set.remove(root.val);
+        else set.add(root.val);
+
+        if (root.left == null && root.right == null) {
+            if (set.size() == 0 || set.size() == 1) {
+                ans += 1;
+            }
         }
-        if (count == 0) return util(data, index+1);
-        if (count == 1) return false;
-        if (count > 4) return false;
-        if (data.length-index < count) return false;
-        int j;
-        for (j = index+1; j < index+count; j++) {
-            if ((data[j] & 128) != 128) return false;
+        util2(root.left, new HashSet<>(set)); util2(root.right, new HashSet<>(set));
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
         }
-        return util(data, j);
     }
 }
