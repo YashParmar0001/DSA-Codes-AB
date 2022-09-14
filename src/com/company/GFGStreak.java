e14a9263ebea7fc61196ba9b75b991096f3037d8
Yash Parmar <yashparmar009@outlook.com>
2022-09-14 17:11:37 +0530

    GFG Streak done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -1,64 +1,70 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GFGStreak {
     public static void main(String[] args) {
-
+        ArrayList<Long> list = new ArrayList<>();
+        list.add(3L); list.add(5L); list.add(7L);
+        System.out.println(numberOfSubsequences(list.size(), list));
     }
 
-    static ArrayList<Integer> arr = new ArrayList<>();
-    static int i=0;
-    public static void convertToMaxHeapUtil(Node root)
-    {
-        i = 0;
-        arr.clear();
-        // inorder traversal to populate 'arr'
-        inorderTraversal(root);
-
-        // BST to MAX HEAP conversion
-        BSTToMaxHeap(root);
-    }
-
-    // Function for the inorder traversal of the tree
-    // so as to store the node values in 'arr' in
-    // sorted order
-    static void inorderTraversal(Node root)
-    {
-        if (root == null)
-            return;
-
-        // first recur on left subtree
-        inorderTraversal(root.left);
-
-        // then copy the data of the node
-        arr.add(root.data);
-
-        // now recur for right subtree
-        inorderTraversal(root.right);
-    }
-
-    static void BSTToMaxHeap(Node root)
-    {
-        if (root == null)
-            return;
-
-        // recur on left subtree
-        BSTToMaxHeap(root.left);
-
-        // recur on right subtree
-        BSTToMaxHeap(root.right);
-
-        // copy data at index 'i' of 'arr' to
-        // the node
-        root.data = arr.get(i++);
-    }
-
-    static class Node {
-        int data;
-        Node left, right;
-        public Node(int data) {
-            this.data = data;
+    static long mod=1000000007;
+    static Long numberOfSubsequences(int N, ArrayList<Long> A){
+        // code here
+        int cnt=0;
+        for(int i=0;i<N;i++){
+            //checking power of 2.
+            if((A.get(i)&A.get(i)-1)==0){
+                cnt++;
+            }
         }
+        //answer goes like this:
+        //nC1 + nC2 + nC3 + ... + nCn = 2^n - 1;
+        long ans=modpwr(2,cnt)-1;
+        Long a=ans;
+        return a;
+    }
+    static long modpwr(long m,long n)
+    {
+        long res=1;
+        m=m%mod;
+        if(m==0)
+            return 0;
+        while(n>0)
+        {
+            if((n&1)!=0)
+            {
+                res=(res*m)%mod;
+            }
+            n=n>>1;
+            m=(m*m)%mod;
+        }
+        return res;
+    }
+
+    static Set<Long> set = new HashSet<>();
+    static ArrayList<Long> ansList = new ArrayList<>();
+
+    static Long numberOfSubsequences1(int N, ArrayList<Long> A){
+        long ans = 0;
+        subseq(A, 0, 1);
+        for (long e : ansList) {
+            double d = Math.log10(e)/Math.log10(2);
+            if (d == (long)d) ans++;
+        }
+        return ans-1;
+    }
+
+    static void subseq(ArrayList<Long> list, int i, long value) {
+        if (i == list.size()) {
+            ansList.add(value);
+            return;
+        }
+
+        subseq(list, i+1, value * list.get(i));
+        subseq(list, i+1, value);
     }
 }
