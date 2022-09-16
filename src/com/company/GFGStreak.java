2db83cff03be4369fc2b290caee538e77393831f
Yash Parmar <yashparmar009@outlook.com>
2022-09-16 07:54:05 +0530

    GFG streak done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -1,27 +1,69 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class GFGStreak {
     public static void main(String[] args) {
-        int[] arr = {20, 2};
-        System.out.println(minSubset(arr, arr.length));
+
     }
 
-    static int minSubset(int[] Arr,int N) {
-        long sum=0;
-        Arrays.sort(Arr);
-        for(int i=0;i<N;i++){
-            sum+=Arr[i];
+    static class Node implements Comparator<Node> {
+        int v;
+        int time;
+        Node(){}
+        Node(int v,int time){
+            this.v = v;
+            this.time = time;
         }
-        long res=0;
-        for(int i=N-1;i>=0;i--){
-            res+=Arr[i];
-            sum-=Arr[i];
-            if(res>sum){
-                return N-i;
+
+        public int compare(Node n1, Node n2){
+            if(n1.time<n2.time){
+                return -1;
+            }
+            if(n1.time>n2.time){
+                return 1;
+            }
+            return 0;
+        }
+    }
+    static int countPaths(int n, List<List<Integer>> roads) {
+        int[] distance = new int[n];
+        int[] ways = new int[n];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+
+        // Building Adj list
+        List<List<Node>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(List<Integer> a: roads){
+
+            int u = a.get(0);
+            int v = a.get(1);
+            int t = a.get(2);
+            adj.get(u).add(new Node(v,t));
+            adj.get(v).add(new Node(u,t));
+        }
+        // Dijkstra's  Algo Starts from here
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(n,new Node());
+        pq.add(new Node(0,0));
+        distance[0] = 0;
+        ways[0] = 1;
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            if(distance[node.v] < node.time)continue;
+            for(Node it:adj.get(node.v)){
+                if(distance[node.v] + it.time < distance[it.v]){
+                    ways[it.v] = ways[node.v];
+                    distance[it.v] = distance[node.v] + it.time;
+                    pq.add(new Node(it.v,distance[it.v]));
+                }
+                else if(distance[node.v] + it.time == distance[it.v]){
+                    ways[it.v] = (ways[it.v] + ways[node.v]);
+                }
             }
         }
-        return N;
+
+        return ways[n-1];
     }
 }
