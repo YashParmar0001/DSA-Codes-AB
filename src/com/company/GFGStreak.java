ea5f8f5bbced1678a0d72c37617c77baf5a16920
Yash Parmar <yashparmar009@outlook.com>
2022-09-20 10:11:15 +0530

    GFG streak done

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -1,27 +1,12 @@
 package com.company;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-
 public class GFGStreak {
     public static void main(String[] args) {
-        int[] arr = {1, 6, 2};
-        System.out.println(leftSmaller(arr.length, arr));
+        System.out.println(numberOfSquares(8));
     }
 
-    static List<Integer> leftSmaller(int n, int arr[]) {
-        List<Integer> ans = new ArrayList<>(n);
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        for (int e : arr) {
-            while (!stack.isEmpty() && stack.peek() >= e) {
-                stack.pop();
-            }
-            if (stack.isEmpty()) ans.add(-1);
-            else ans.add(stack.peek());
-
-            stack.push(e);
-        }
-        return ans;
+    public static long numberOfSquares(long base) {
+        long n = (base - 2)/2L;
+        return (n*(n+1))/2L;
     }
 }
---
+++ src/com/company/Graphs/Implementation.java
@@ -0,0 +1,96 @@
+package com.company.Graphs;
+
+import java.util.*;
+
+public class Implementation {
+    public static void main(String[] args) {
+        int v = 4;
+        GraphAdjList graph = new GraphAdjList(v);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+        boolean[] visited = new boolean[v];
+        GraphAdjList.dfsStack(0, visited);
+        System.out.println();
+        GraphAdjList.dfs(0, new boolean[v]);
+//        System.out.println();
+//        visited = new boolean[v];
+//        GraphAdjList.bfs(0, visited);
+    }
+}
+
+class GraphAdjList {
+    static List<List<Integer>> list = new ArrayList<>();
+    int v;
+
+    public GraphAdjList(int v) {
+        this.v = v;
+        for (int i = 0; i < v; i++) {
+            list.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        list.get(src).add(dest);
+        list.get(dest).add(src);
+    }
+
+    public static void dfs(int src, boolean[] visited) {
+        visited[src] = true;
+        System.out.print(src + ", ");
+
+        for (int neighbour : list.get(src)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, visited);
+            }
+        }
+    }
+
+    public static void bfs(int src, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+        visited[src] = true;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + ", ");
+            for (int neighbour : list.get(current)) {
+                if (!visited[neighbour]) {
+                    queue.offer(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        }
+    }
+
+    public static void dfsStack(int src, boolean[] visited) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(src);
+        visited[src] = true;
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            System.out.print(current + ", ");
+            for (int neighbour : list.get(current)) {
+                if (!visited[neighbour]) {
+                    stack.push(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        }
+    }
+}
+
+class GraphAdjMatrix {
+    int[][] matrix;
+    int v;
+
+    public GraphAdjMatrix(int v) {
+        this.v = v;
+        matrix = new int[v][v];
+    }
+
+    public void addEdge(int src, int dest) {
+        matrix[src][dest] = 1;
+        matrix[dest][src] = 1; // For undirected graph
+    }
+}
