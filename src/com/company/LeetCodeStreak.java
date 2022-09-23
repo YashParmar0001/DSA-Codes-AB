9b051c0ced5a46efe286dec9ebb1607b8c090a71
Yash Parmar <yashparmar009@outlook.com>
2022-09-23 17:32:22 +0530

    Leetcode streak done!

--- src/com/company/LeetCodeStreak.java
+++ src/com/company/LeetCodeStreak.java
@@ -1,41 +1,16 @@
 package com.company;
 
-import java.util.*;
-
 public class LeetCodeStreak {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        int[][] queries = {{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
-        System.out.println(Arrays.toString(sumEvenAfterQueries(nums, queries)));
+        System.out.println(concatenatedBinary(12));
     }
 
-    static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-        nums[queries[0][1]] = nums[queries[0][1]] + queries[0][0];
-        int[] ans = new int[nums.length]; int index = 1;
-        int sum = 0;
-        for (int e : nums) {
-            if (e % 2 == 0) {
-                sum += e;
-            }
+    static int concatenatedBinary(int n) {
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int bitLen = (int) (Math.log(i) / Math.log(2)) + 1;
+            ans = ((ans << bitLen) + i) % 1000000007;
         }
-        ans[0] = sum;
-        for (int i = 1; i < queries.length; i++) {
-            int[] querie = queries[i];
-            int n = nums[querie[1]];
-            nums[querie[1]] = nums[querie[1]] + querie[0];
-            if (n % 2 == 0) {
-                if (nums[querie[1]] % 2 == 0) {
-                    sum -= n;
-                    sum += nums[querie[1]];
-                }else {
-                    sum -= n;
-                }
-            }else {
-                if (nums[querie[1]] % 2 == 0) sum += nums[querie[1]];
-            }
-            ans[index] = sum;
-            index++;
-        }
-        return ans;
+        return (int) ans;
     }
 }
