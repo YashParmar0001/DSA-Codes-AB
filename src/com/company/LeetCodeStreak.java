14bd598ddaed17a657f372731348d70dce9cef0f
Yash Parmar <yashparmar009@outlook.com>
2022-09-24 10:48:12 +0530

    Leetcode streak done!

--- src/com/company/LeetCodeStreak.java
+++ src/com/company/LeetCodeStreak.java
@@ -1,16 +1,45 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCodeStreak {
     public static void main(String[] args) {
-        System.out.println(concatenatedBinary(12));
+        TreeNode root = new TreeNode(-2);
+        root.left = new TreeNode(-3);
+        System.out.println(pathSum(root, -5));
     }
 
-    static int concatenatedBinary(int n) {
-        long ans = 0;
-        for (int i = 1; i <= n; i++) {
-            int bitLen = (int) (Math.log(i) / Math.log(2)) + 1;
-            ans = ((ans << bitLen) + i) % 1000000007;
+    static List<List<Integer>> ans = new ArrayList<>();
+    static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        util(root, 0, targetSum, new ArrayList<>());
+        return ans;
+    }
+
+    static void util(TreeNode root, int sum, int targetSum, List<Integer> currList) {
+        if (root == null) {
+            return;
         }
-        return (int) ans;
+        sum += root.val;
+        currList.add(root.val);
+        if (sum == targetSum && isLeaf(root)) {
+            ans.add(currList);
+            return;
+        }
+
+        util(root.left, sum, targetSum, new ArrayList<>(currList));
+        util(root.right, sum, targetSum, new ArrayList<>(currList));
+    }
+
+    static boolean isLeaf(TreeNode root) {
+        return root.left == null && root.right == null;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+        }
     }
 }
