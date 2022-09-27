cb3d71f95da11412104a47d0b6672eded9f2675c
Yash Parmar <yashparmar009@outlook.com>
2022-09-27 12:22:57 +0530

    GFG streak done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -1,39 +1,24 @@
 package com.company;
 
+import java.util.PriorityQueue;
+
 public class GFGStreak {
     public static void main(String[] args) {
 
     }
 
-    public int[][] solveQueries(int n, int[][] Queries) {
-        // Code here
-        int a[][]=new int[n][n];
-        for (int[] query : Queries) {
-            int x1 = query[0];
-            int y1 = query[1];
-            int x2 = query[2];
-            int y2 = query[3];
-            a[x1][y1]++;
-            if (x2 < n - 1) {
-                a[x2 + 1][y1]--;
-            }
-            if (y2 < n - 1) {
-                a[x1][y2 + 1]--;
-            }
-            if (x2 < n - 1 && y2 < n - 1) {
-                a[x2 + 1][y2 + 1]++;
-            }
+    long minCost(long arr[], int n) {
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        for (long e : arr) {
+            queue.offer(e);
         }
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                a[i][j]+=a[i][j-1];
-            }
+        long cost = 0;
+        while (queue.size() > 1) {
+            long first = queue.poll();
+            long second = queue.poll();
+            cost += first + second;
+            queue.offer(first + second);
         }
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                a[j][i]+=a[j-1][i];
-            }
-        }
-        return a;
+        return cost;
     }
 }
