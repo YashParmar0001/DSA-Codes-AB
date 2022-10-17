99719f5cafc5541c71c993a92a0c6733639d84f3
Yash Parmar <yashparmar009@outlook.com>
2022-10-17 10:36:20 +0530

    GFG streak done!

--- src/com/company/GFGStreak.java
+++ src/com/company/GFGStreak.java
@@ -1,7 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
+
 public class GFGStreak {
     public static void main(String[] args) {
+        int[] arr = {8, 58, 71, 18, 31, 32, 63, 92, 43, 3, 91, 93, 25, 80, 28};
+        System.out.println(findLeastGreater(arr.length, arr));
+    }
 
+    public static ArrayList<Integer> findLeastGreater(int n, int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        int[] ansArr = new int[n];
+        for (int i = n-1; i >= 0; i--) {
+            if (set.ceiling(arr[i]+1) == null) {
+                ansArr[i] = -1;
+            }else {
+                ansArr[i] = set.ceiling(arr[i]+1); // This works like an upper bound
+            }
+            set.add(arr[i]);
+        }
+        for (int e : ansArr) ans.add(e);
+        return ans;
     }
 }
